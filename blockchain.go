d5e7218413e75ccb94d7b2f7c3c8783a50b46bc7
OcheOps <ochecodes@gmail.com>
2023-11-30 08:19:17 +0100

    first commit

---
+++ blockchain.go
@@ -0,0 +1,22 @@
+package main 
+
+import (
+	"fmt"
+
+)
+
+func init(){
+	log.SetPrefix("Blockchain:")
+}
+
+func main ()  {
+	log.Println("test 2")
+	fmt.Println("test")
+
+}
+
+
+//func init(){
+	log.SetPrefix("Blockchain:")
+//}
+
